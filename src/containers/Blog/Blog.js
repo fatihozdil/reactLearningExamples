16dd88c0426908c9ff60e90dd201a20b65a4ec7a
Muhammed Fatih ÖZDİL <muhammedfatihozdil@gmail.com>
2021-01-12 13:35:15 +0300

    different way of redirecting added using router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,7 +5,9 @@ import { Route, NavLink, Switch,Redirect } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-
+state={
+    auth: false
+}
     render() {
 
         return (
@@ -31,8 +33,10 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={()=> <h1>Home</h1>}/>
                   <Route path="/" render={()=> <h1>Home</h1>}/> */}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth?<Route path="/new-post" component={NewPost} />:null}
                     <Route path="/posts/"  component={Posts} />
+                    {/* <Route render={()=> <h1>not found</h1>}/> */} {/* we can use this way to redirect or render something end of the route path 
+                    but we cant use this redirecting way with redirect component */}
                     <Redirect from="/" to="/posts"/>
                   {/*   <Route path="/"  component={Posts} /> */} {/* to redirect user to posts even if the user is in the root directory */}
                     
