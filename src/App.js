8277de4d0616e4f8bfdf1369d4556cdff0d77960
Muhammed Fatih ÖZDİL <muhammedfatihozdil@gmail.com>
2021-02-12 11:11:21 +0300

    Routing and server deployment: If our base name is different from '/', we must tell React the base name to avoid server 404 errors

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+         // <BrowserRouter basename="my-app"> If our base name is different from '/', we must tell React the base name to avoid server 404 errors
       <BrowserRouter>
         <div className="App">
           <Blog />
