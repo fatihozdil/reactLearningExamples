bd777e3f8e5662d4ea6a9cd35ff15b74ce90b6ad
Muhammed Fatih ÖZDİL <muhammedfatihozdil@gmail.com>
2021-02-12 10:07:51 +0300

    add lazy loading: when using rooting, the page is downloaded unnecessary, even if the user never go to the referred page. we use lazy loading to avoid that

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import { Route, NavLink, Switch,Redirect } from 'react-router-dom';
-import NewPost from './NewPost/NewPost';
+//import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost')
+})
 class Blog extends Component {
 state={
-    auth: false
+    auth: true
 }
     render() {
 
@@ -33,7 +37,7 @@ state={
                 {/* <Route path="/" exact render={()=> <h1>Home</h1>}/>
                   <Route path="/" render={()=> <h1>Home</h1>}/> */}
                 <Switch>
-                    {this.state.auth?<Route path="/new-post" component={NewPost} />:null}
+                    {this.state.auth?<Route path="/new-post" component={AsyncNewPost} />:null}
                     <Route path="/posts/"  component={Posts} />
                     {/* <Route render={()=> <h1>not found</h1>}/> */} {/* we can use this way to redirect or render something end of the route path 
                     but we cant use this redirecting way with redirect component */}
